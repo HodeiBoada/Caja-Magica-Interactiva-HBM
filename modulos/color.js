92886efd04563099157c19065a71573b777790ec
BuseifM <buseifelcrack11@gmail.com>
2025-12-05 18:42:22 +0100

    creando la funcion cambiar color

---
+++ modulos/color.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function cambiarColor(){
+    let opciones = ["azul", "verde", "rojo", "amarillo", "rosa", "azul"];
+    let btnColor = document.getElementById("btnColor");
+    let miCaja = document.getElementById("miCaja");
+    let i = 0;
+}
+
+export {cambiarColor};
