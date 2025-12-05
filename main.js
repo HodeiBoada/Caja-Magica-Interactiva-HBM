c691bda76b8dff8699e41bd718640961165efa7b
Miguel853835 <msedanoi01@educantabria.es>
2025-12-05 18:51:32 +0100

    a ver ahora

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 `use strict`;
-import {cambiarTexto} from './modulos/texto.js';
-import {cambiarColor} from './modulos/color.js';
+//import {cambiarTexto} from './modulos/texto.js';
+//import {cambiarColor} from './modulos/color.js';
 import {cambiarTamano} from './modulos/tamano.js';
 
 window.addEventListener("DOMContentLoaded", inicio);
