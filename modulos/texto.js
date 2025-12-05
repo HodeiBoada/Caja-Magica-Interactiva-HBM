28398f684370f70b4b7d25223b6b15876680c1eb
Miguel853835 <msedanoi01@educantabria.es>
2025-12-05 18:19:46 +0100

    Cambio de texto

--- modulos/texto.js
+++ modulos/texto.js
@@ -1,1 +1,17 @@
+`use strict`;
+
+function cambiarTexto(){
+    let opciones = ["¡Hola!","¿Cómo estás?","Aprendiendo DOM","DOM Aprendido", "¡Adiós!"]
+    let btnTexto = document.getElementById("btnTexto");
+    let miCaja = document.getElementById("miCaja");
+    let i = 0;
+    btnTexto.addEventListener("click", ()=>{
+        miCaja.textContent = opciones[i];
+        i = (i + 1) % opciones.length;
+        
+    })
+}
+
+export {cambiarTexto};
+
 
