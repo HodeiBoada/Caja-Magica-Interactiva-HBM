b1fb82dcc3f886be16fc7c39b3ecf7b5464c4710
Miguel853835 <msedanoi01@educantabria.es>
2025-12-05 18:45:39 +0100

    ahora si que si

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import {cambiarTexto} from './modulos/texto.js';
 import {cambiarColor} from './modulos/color.js';
 import {cambiarTamano} from './modulos/tamano.js';
 
-window.reload = inicio;
+window.onload = inicio;
 
 function inicio(){
     cambiarTexto();
