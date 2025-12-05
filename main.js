079243ab567fc96d03a362218f773362ef24f356
Miguel853835 <msedanoi01@educantabria.es>
2025-12-05 18:47:17 +0100

    ahoraaaaaa

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import {cambiarTexto} from './modulos/texto.js';
 import {cambiarColor} from './modulos/color.js';
 import {cambiarTamano} from './modulos/tamano.js';
 
-window.onload = inicio;
+window.addEventListener("DOMContentLoaded", inicio);
 
 function inicio(){
     cambiarTexto();
